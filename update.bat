dd0a5706a4538501aa325cfe3356eab8d1c58131
wq <wq@votzone.com>
2018-07-14 22:13:48 +0800

    importent git update repo

---
+++ update.bat
@@ -0,0 +1,42 @@
+cd ..
+
+cd Apktool
+git pull
+cd ..
+
+cd ItChat
+git pull
+cd ..
+
+cd VirtualApp
+git pull
+cd ..
+
+
+cd AssetStudio
+git pull
+cd ..
+
+cd ShakaApktool
+git pull
+cd ..
+
+cd awesome-design-cn
+git pull
+cd ..
+
+cd jadx
+git pull
+cd ..
+
+cd Il2CppDumper
+git pull
+cd ..
+
+cd TurboDex
+git pull
+cd ..
+
+cd dnSpy
+git pull
+cd ..
