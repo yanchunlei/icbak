ea467e5e4f5549fac3ce0608a4e48e79027c83fb
wq <wq@votzone.com>
2018-07-15 13:31:15 +0800

    add more git repo

--- update.bat
+++ update.bat
@@ -1,27 +1,7 @@
 cd ..
 
+cd androidsecurity
 cd Apktool
-git pull
-cd ..
-
-cd ItChat
-git pull
-cd ..
-
-cd VirtualApp
-git pull
-cd ..
-
-
-cd AssetStudio
-git pull
-cd ..
-
-cd ShakaApktool
-git pull
-cd ..
-
-cd awesome-design-cn
 git pull
 cd ..
 
@@ -29,15 +9,104 @@ cd jadx
 git pull
 cd ..
 
-cd Il2CppDumper
+cd dexdump
+git pull
+cd ..
+
+cd ShakaApktool
+git pull
+cd ..
+
+cd Bangcle
+git pull
+cd ..
+cd ..
+
+cd elfhook
+cd AllHookInOne
 git pull
 cd ..
 
 cd TurboDex
 git pull
 cd ..
+cd ..
+
+cd unity
+cd AssetStudio
+git pull
+cd ..
+
+
+cd Il2CppDumper
+git pull
+cd ..
 
 cd dnSpy
+git pull
+cd ..
+cd ..
+
+cd xposted
+cd android_art
+git pull
+cd ..
+
+cd Xposed
+git pull
+cd ..
+
+cd XposedBridge
+git pull
+cd ..
+
+cd XposedInstaller
+git pull
+cd ..
+
+cd XposedTools
+git pull
+cd ..
+cd ..
+
+cd androidplugin
+cd VirtualApp
+git pull
+cd ..
+
+cd RePlugin
+git pull
+cd ..
+
+cd DroidPlugin
+git pull
+cd ..
+cd ..
+
+cd MCPE
+cd MCPELauncher
+git pull
+cd ..
+
+cd aFileChooser
+git pull
+cd ..
+
+cd libsuperuser
+git pull
+cd ..
+
+cd MCPELauncher-app
+git pull
+cd ..
+
+cd ..
+
+cd ItChat
+git pull
+cd ..
+
+cd awesome-design-cn
 git pull
 cd ..
 
